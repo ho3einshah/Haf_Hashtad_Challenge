c946818f170dee3da8b6143df4c3b2eb853e0440
ho3einshah <ho3einshah1375@gmail.com>
2024-03-15 09:54:32 +0330

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Haf Hashtad Code Challenge"
+include(":app")
